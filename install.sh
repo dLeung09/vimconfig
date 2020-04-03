e6000f1df38f7bd2e591bd88641afa92f4893070
David Leung <davidaleung+github@gmail.com>
2020-04-03 09:32:58 -0700

    Update install script; configure tmux for colors
    - configures git email and user name
    - configures git core editor
    - adds executable permission to install script
    - updates tmux config to allow for colours in sessions

--- install.sh
+++ install.sh
@@ -113,3 +113,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.st status
 git config --global alias.unstage 'reset HEAD --'
+
+git config --global user.email "davidaleung+github@gmail.com"
+git config --global user.name "David Leung"
+git config --global core.editor vim
